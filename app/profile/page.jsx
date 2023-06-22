2f02ba605a38c14dc2bca72eaf0589788f570a28
Sikarev <sikarev.roman@yandex.ru>
2023-06-22 16:05:09 +0300

    CRUD for prompt posts

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -4,11 +4,14 @@ import { useState, useEffect } from 'react';
 import { useSession } from "next-auth/react";
 
 import Profile from '@components/Profile';
+import { useRouter } from "next/navigation";
 
 const MyProfile = () => {
   const { data: session } = useSession();
 
   const [posts, setPosts] = useState([]);
+
+  const router = useRouter();
 
 
   useEffect(() => {
@@ -23,11 +26,26 @@ const MyProfile = () => {
     }
   }, []);
 
-  const handleEdit = () => {
-
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`)
   }
-  const handleDelete = () => {
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm('Are you sure you want to delete this prompt?');
 
+    if (!hasConfirmed) {
+      return Promise.resolve();
+    }
+
+    try {
+      await fetch(`api/prompt/${post._id.toString()}`, {
+        method: 'DELETE',
+      });
+
+      const filteredPosts = posts.filter(p => p._id !== post._id);
+      setPosts(filteredPosts);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
